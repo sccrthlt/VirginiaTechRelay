ce248434e6771925012a0b4a7951c1de3a22133b
Scott Welch <scott2@vt.edu>
2013-02-26 00:34:58 -0500

    Testing what newlist returns in order to get position

--- relay/relayapp/views.py
+++ relay/relayapp/views.py
@@ -95,10 +95,11 @@ def all_team_candles_general(request):
 
 	newlist = sorted(all_team_candles, key=itemgetter('team_candles_total'), reverse = True)
 	
-	currPos = 1
+	#currPos = 1
 	for item in newlist:
-		candles['pos'] = currPos
-		currPos = currPos + 1
+		print item
+		#candles['pos'] = currPos
+		#currPos = currPos + 1
 
 	response = json.dumps(newlist)
 	return HttpResponse(response, mimetype="application/json")
