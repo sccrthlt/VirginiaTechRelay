908364b7fbbda892f265d8648fb91742355dd4f6
Scott Welch <scott2@vt.edu>
2013-03-03 14:46:00 -0500

    QUerying

--- relay/reports/management/commands/hokie_passport.py
+++ relay/reports/management/commands/hokie_passport.py
@@ -25,7 +25,7 @@ def setupEventRecord(info):
 				new_participant_event_record = Participant_Event_Record(
 					participant = Participant.objects.get(hokie_passport_id = info['Card number']),
 					id = info['Card number'],
-					event = model_to_dict(Event.objects.get(pk = this_event))['name'],
+					event = model_to_dict(Event.objects.get(event = this_event))['name'],
 				)
 				new_participant_event_record.save()
 			except len(info['Card number']) == 0:
