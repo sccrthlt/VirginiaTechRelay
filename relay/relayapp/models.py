fa17845853da113ecd8c1aa4ff55b49f5c7fb2b6
Scott Welch <scott2@vt.edu>
2013-03-10 16:40:00 -0400

    Installing django_facebook

--- relay/relayapp/models.py
+++ relay/relayapp/models.py
@@ -2,6 +2,25 @@
 from django.db import models
 from decimal import Decimal
 
+from django_facebook.models import FacebookProfileModel
+
+
+class MyCustomProfile(FacebookProfileModel):
+    user = models.OneToOneField('auth.User')
+    ....
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+#Make sure we create a MyCustomProfile when creating a User
+def create_facebook_profile(sender, instance, created, **kwargs):
+    if created:
+        MyCustomProfile.objects.create(user=instance)
+
+post_save.connect(create_facebook_profile, sender=User)
+
+
+
 class Company(models.Model):
 
     RELAY = 'RT'
