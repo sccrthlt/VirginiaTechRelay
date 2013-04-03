31916808f9c4bcd2776e881ccab835302869cc05
Scott Welch <scott2@vt.edu>
2013-04-03 15:42:40 -0400

    Took out sponsor out of pledge model

--- relay/relayapp/models.py
+++ relay/relayapp/models.py
@@ -197,7 +197,6 @@ class Olympics_Lap_Counter_Signup(models.Model):
 	datetime = models.DateTimeField(default=None, blank=True, null=True)
 
 class Pledge(models.Model):
-	sponsor = models.CharField(max_length=100, default='cool', blank=True, null=True)
 	participant = models.ForeignKey('Participant', default=None, blank=True, null=True)
 	pledge_amount = models.DecimalField(default=Decimal('0.00'), max_digits=10, decimal_places=2, blank=True)
 	max_pledge_amount = models.DecimalField(default=Decimal('0.00'), max_digits=10, decimal_places=2, blank=True)
